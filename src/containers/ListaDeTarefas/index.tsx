1bb3d98f4805712c8815f9f37c3c00627a5490f0
Matheus Meissner <matheus.iembo@gmail.com>
2024-07-25 16:42:41 -0300

    corrige filtros

--- src/containers/ListaDeTarefas/index.tsx
+++ src/containers/ListaDeTarefas/index.tsx
@@ -2,13 +2,40 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 import Tarefa from '../../components/Tarefa'
 import { RootReducer } from '../../store'
+import TarefaClass from '../../models/Tarefa'
 
 const ListaDeTarefas = () => {
-  const tarefas = useSelector((state: RootReducer) => state.tarefas.itens)
+  const { filtro, tarefas } = useSelector((state: RootReducer) => state)
+
+  const filtrarTarefas = () => {
+    let tarefasFiltradas = tarefas.itens
+
+    if (filtro.termo) {
+      tarefasFiltradas = tarefasFiltradas.filter((tarefa: TarefaClass) =>
+        tarefa.titulo.toLowerCase().includes(filtro.termo!.toLowerCase())
+      )
+    }
+
+    if (filtro.criterio === 'prioridade' && filtro.valor) {
+      tarefasFiltradas = tarefasFiltradas.filter(
+        (tarefa) => tarefa.prioridade === filtro.valor
+      )
+    }
+
+    if (filtro.criterio === 'status' && filtro.valor) {
+      tarefasFiltradas = tarefasFiltradas.filter(
+        (tarefa) => tarefa.status === filtro.valor
+      )
+    }
+
+    return tarefasFiltradas
+  }
+
+  const tarefasFiltradas = filtrarTarefas()
 
   return (
     <div>
-      {tarefas.map((t) => (
+      {tarefasFiltradas.map((t) => (
         <Tarefa
           key={t.id}
           id={t.id}
